bfbf52e53815453761f1b78ee9bce66e057b5cce
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-08-17 09:38:33 -0300

    finish

---
+++ src/screens/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Users from '../components/Users';
+
+const HomeScreen = props => {
+  return (
+    <Users navigation={props.navigation}/>
+  );
+};
+
+HomeScreen.navigationOptions = {
+  title: 'User List'
+}
+
+export default HomeScreen;
